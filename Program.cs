5e4814cc078a2de9fb26056c92ad25bf9b84ba87
kshrhrhrhr <d34k03a221@icloud.com>
2025-12-13 00:54:41 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+using System;
+
+class Program
+{
+    
+}
